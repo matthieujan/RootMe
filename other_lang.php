7816e7ef84da022b60beea442bb9d902f39a7e4e
Matthieu Jan <matthieujan@protonmail.com>
2017-11-28 00:13:31 +0100

    Update other_lang.php

--- other_lang.php
+++ other_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang = array (
-            'lang' => include 'php://filter/convert.base64-encode/resource=index.php',
+            'lang' => php://filter/convert.base64-encode/resource=index.php,
             'welcome' => 'Bienvenue sur notre nouveau site web !',
         );
 ?>
